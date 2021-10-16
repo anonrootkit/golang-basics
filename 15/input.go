0827c625930399b5aa5197848fbcbd3306b35f2a
Ankit Gupta <ankitg0000640@gmail.com>
2021-10-16 16:56:48 +0530

    Bufio example added

---
+++ 15/input.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var inputSize = 1000000
+
+func main() {
+
+	var input *bufio.Scanner
+	init_input_scanner(&input)
+
+	init_header()
+
+	var username string = "Ankit"
+	takeInput(&username, *input)
+
+	welcome_user(username)
+}
+
+func init_input_scanner(input **bufio.Scanner) {
+	*input = bufio.NewScanner(os.Stdin)
+	(*input).Buffer(make([]byte, inputSize), inputSize)
+}
+
+func init_header() {
+	fmt.Print("Hey there!\n")
+	fmt.Print("Let us know who you are : ")
+}
+
+func welcome_user(name string) {
+	fmt.Println("Welcome, " + name)
+}
+
+func takeInput(obj *string, input bufio.Scanner) {
+	input.Scan()
+	fmt.Scanln(input.Text(), obj)
+}
